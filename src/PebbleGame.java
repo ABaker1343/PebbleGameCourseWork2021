8b2ba2645d8efed4e939fea984df4db365f61ba6
Alex <billybo1234@outlook.com>
2021-11-02 13:54:13 +0000

    fleshed out template to include methods

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+
 public class PebbleGame {
+
+    Bag [] bags;
+    Player [] players;
 
     public void init(){
         //TODO get amount of players as input from the player
@@ -19,6 +24,33 @@ public class PebbleGame {
     }
 
     class Player implements Runnable {
+        Stone [] hand;
+
+        public Player(){
+
+        }
+
+        public void drawHand(Bag bag){
+            //TODO implement a method to draw a full hand from a bag
+            //bag here is passed as a pointer
+        }
+
+        public void drawStone(){
+            //TODO implement drawing one stone
+        }
+
+        public void discardStone(){
+            //TODO implement discarding a stone from the players hand
+            //this should be doable with pointers and the remove() function in ArrayLists
+            //if not can be done using indexs in the players hand
+        }
+
+        private boolean hasWon(){
+            //TODO check if the hand is at the 100 total value to win
+            //otherwise continue
+            return false;
+        }
+
         @Override
         public void run(){
             //TODO if player hand is equal to 100 intterupt all other player threads
@@ -28,10 +60,44 @@ public class PebbleGame {
     }
 
     class Bag {
-        public void drawStone(){
+        
+        //in the bag class we will have to make sure that only one player can
+        //access the bag at any given time, this is to ensure that players are not
+        //operating on stale data
+
+        ArrayList<Stone> stones;
+
+        public Bag(){
+            //initialize a new arraylist of stones
+            stones = new ArrayList<Stone>();
+        }
+
+        public void takeStone(){
             //TODO if bag has stones remaining draw one and return it
 
             //TODO otherwise draw all stones from corresponding white bag
         }
+
+        public void addStone(Stone stone){
+            //TODO add the stone that is passed into the method to the list of stones
+            //be careful here because the stone is passed in via pointer so make
+        }
+
+        public void transferStones(Bag bag){
+            //TODO move all the stones in this bag to a bag passed in through the arguments
+        }
+    }
+
+    class Stone {
+        final int value;
+
+        public Stone(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
     }
 }
---
+++ src/PebbleGameTester.java
@@ -0,0 +1,9 @@
+class PebbleGameTester {
+
+    //class for testing to keep test code away from rest of the code
+    //keeps it more consise
+
+    PebbleGameTester() {
+
+    }
+}
