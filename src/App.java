f2aabd467a4170352fcd7b2bff3f796976c32bfd
Alex <billybo1234@outlook.com>
2021-11-02 11:32:56 +0000

    created template for logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PebbleGame game = new PebbleGame();
+        game.init();
+        game.run();
     }
 }
